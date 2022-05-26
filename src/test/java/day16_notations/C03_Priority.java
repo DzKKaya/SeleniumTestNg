0365dd15a1705e1b6e15cf42b0375a3f0581d239
DzKKaya <kayalik007@gmail.com>
2022-05-27 00:13:36 +0300

    26.05.2022

---
+++ src/test/java/day16_notations/C03_Priority.java
@@ -0,0 +1,21 @@
+package day16_notations;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_Priority extends TestBase {
+
+
+    @Test (priority = -5)
+    public void amazonTesti() {
+        driver.get("https://www.amazon.com");
+    }
+    @Test (priority = -2)
+    public void test02() {
+        driver.get("https://www.bestbuy.com");
+    }
+    @Test
+    public void techproedTesti() {
+        driver.get("https://www.techproeducation.com");
+    }
+}
