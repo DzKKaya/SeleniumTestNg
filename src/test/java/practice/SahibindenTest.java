76e9eb2bce7095f64553b2816a860b5fff8e6541
DzKKaya <kayalik007@gmail.com>
2022-06-05 00:56:38 +0300

    05.06.2022

--- src/test/java/practice/SahibindenTest.java
+++ src/test/java/practice/SahibindenTest.java
@@ -1,15 +1,10 @@
 package practice;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.SahibindenPage;
 import utilities.Driver;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
